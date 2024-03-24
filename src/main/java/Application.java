8683293402cbfff07c4a84d673760f6c95f4e126
inthejim <inthejim24@gmail.com>
2024-03-25 08:16:13 +0900

    feat : 게임 끝 문구 출력

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,15 @@
 public class Application {
     private final static int END_VALUE=2;
-
+    private static InputView input = new InputView();
+    private static OutputView output=new OutputView();
     public static void main(String[] args){
-        InputView input = new InputView();
         boolean play=true;
         while(play){
             GameController game=new GameController();
             while(!game.isEnded()){
                 game.playTurn();
             }
+            output.printAnswerSituation();
             if(input.getRestartOrEnd()==END_VALUE){
                 play=false;
             }
