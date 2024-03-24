6d91d715e33d491598a09e147e0b38e11a796dcd
inthejim <inthejim24@gmail.com>
2024-03-24 21:59:16 +0900

    rename: Number->NumberGenerator

--- src/main/java/NumberGenerator.java
+++ src/main/java/NumberGenerator.java
@@ -1,13 +1,8 @@
 import java.util.Random;
 
-public class Number {
-    private int number;
+public class NumberGenerator {
     private static final int SMALLEST_NUMBER=1;
     private static final int LARGEST_NUMBER=9;
-
-    public Number(int n){
-        this.number=n;
-    }
 
     private boolean validateNumber(int number){
         if(SMALLEST_NUMBER<=number && number<=LARGEST_NUMBER){
